ecf9c1e571954209d72de969720cd10b731bee2a
Nick <ma.suslov@gmail.com>
2024-05-28 15:15:30 +0300

    reboot

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic Node Server
+
+Demo code from https://tannerdolby.com/writing/build-a-basic-http-server-with-node/
---
+++ handlers.js
@@ -0,0 +1,60 @@
+const fs = require("fs");
+
+const handlerObj = {
+    "/": home,
+    "/message": message
+};
+
+function home(res) {
+    console.log("Executing 'home' handler");
+    fs.readFile(__dirname + "/index.html", (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+    });
+}
+
+function message(res, payload) {
+    console.log("Executing 'message' handler");
+    let msg = new URLSearchParams(payload);
+    let data = {
+        pass: msg.get('pass'),
+        apiKey: msg.get('apiKey'),
+        siteName: msg.get('siteName'),
+        net2IP: msg.get('net2IP'),
+        net2ApiBaseUrl: msg.get('net2ApiBaseUrl'),
+        net2ApiHubUrl: msg.get('net2ApiHubUrl'),
+        net2User: msg.get('net2User'),
+        net2Pass: msg.get('net2Pass'),
+        net2ClientId: msg.get('net2ClientId'),
+    };
+
+    require('reboot').reboot();
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(data));
+}
+
+module.exports = { 
+    home, 
+    message,
+    handlerObj
+};
+
+
+/*
+Steps to up and run site:
+
+1 - generate settings file
+2 - register site
+3 - reboot system
+
+Preparation steps: 
+
+1 - services should have run condition
+2 - site run service should also have run conditions
+
+*/
