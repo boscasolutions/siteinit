7f47ccd27e293a97895121793e1310013ee4f729
Nick <ma.suslov@gmail.com>
2024-06-01 19:40:42 +0300

    fix

--- generate-cert.sh
+++ generate-cert.sh
@@ -4,4 +4,5 @@ openssl pkey -in key.pem -out net2.key
 openssl crl2pkcs7 -nocrl -certfile cert.pem | openssl pkcs7 -print_certs -out net2.crt
 sudo cp cert.pem /home/bosca/cert.pem
 sudo cp key.pem /home/bosca/key.pem
+sudo cp cert.pem /usr/local/share/ca-certificates/local-ca.crt
 sudo update-ca-certificates
